7ceec5b540e8d4244445b2e9c547c93a8315897a
Adhaliel <santiagourregou.4@gmail.com>
2024-01-22 16:47:10 -0500

    database and create user

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from "express"
 import morgan from "morgan"
 
+import authRoutes from './routes/auth.routes.js'
+
 const app = express()
 app.use(morgan('dev'))
+
+// console.log(authRoutes);
+// app.use(authRoutes);
+// console.log(app);
 
 export default app
 
