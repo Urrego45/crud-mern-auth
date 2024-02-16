6b9da2d210483f7232c2970948c201c14cb42016
Adhaliel <santiagourregou.4@gmail.com>
2024-02-16 11:59:16 -0500

    adding mongodb atlas

--- src/db.js
+++ src/db.js
@@ -2,13 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
-            authSource: `${process.env.MONGO_USERNAME}`,
-            user: `${process.env.MONGO_USERNAME}`,
-            pass: `${process.env.MONGO_PASSWORD}`,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.URL_MONGO_ATLAS)
         console.log('>>> DB coneccted');
     } catch (error) {
         console.log(error)
